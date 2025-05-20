67db92d7ae2a44e420132a43e8475deef1cb9899
chappa <108088135+chapppington@users.noreply.github.com>
2025-05-20 13:08:11 +0700

    fuck this custom scrollbar idk its buggy af

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import ConditionalMainScene from "@/components/3DScene/ConditionalMainScene";
 import { CameraProvider } from "@/components/3DScene/features/CameraContext";
 import { ViewTransitions } from "next-view-transitions";
 import { TransitionProvider } from "@/components/providers/TransitionProvider";
-import CustomScrollbar from "@/components/ui/CustomScrollbar";
 
 const inter = Inter({
   subsets: ["cyrillic"],
@@ -41,7 +40,6 @@ export default function RootLayout({
           <TransitionProvider>
             <CameraProvider>
               <ConditionalBGGradient />
-              <CustomScrollbar />
               <ConditionalMainScene />
               <BreakpointIndicator />
               <Navbar />
