fc73f636ca51281595393ab51b44fac571d10776
chappa <108088135+chapppington@users.noreply.github.com>
2025-05-31 11:40:53 +0700

    utils small refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ViewTransitions } from "next-view-transitions";
-import { BreakpointIndicator } from "@/components/dev/BreakpointIndicator";
+import { BreakpointIndicator } from "@/utils/BreakpointIndicator";
 
 import Navbar from "@/components/shared_screens/Navbar";
 import Footer from "@/components/shared_screens/Footer";
