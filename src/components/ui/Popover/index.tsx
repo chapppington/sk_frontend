0e487c79464e7946b155faf315cf15d17b762d63
chappa <108088135+chapppington@users.noreply.github.com>
2025-06-09 13:44:07 +0700

    fix popover v2

--- src/components/ui/Popover/index.tsx
+++ src/components/ui/Popover/index.tsx
@@ -30,9 +30,9 @@ const Popover: FC<IPopoverProps> = ({
       ref={popoverRef}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      className={`absolute top-1/2  right-full w-80 max-w-[calc(100vw-2rem)] p-3 bg-white/10 backdrop-blur-xl rounded-lg text-white text-sm z-50 invisible opacity-0 select-none
-      md:right-full
-      sm:right-full sm:mr-2 ${className}`}
+      className={`absolute right-full top-1/2 -translate-y-1/2 mr-2 w-80 max-w-[calc(100vw-2rem)] p-3 bg-white/10 backdrop-blur-xl rounded-lg text-white text-sm z-50 invisible opacity-0 select-none
+      md:right-full md:translate-x-0
+      sm:right-1/2 sm:-translate-x-1/2 sm:mr-0 ${className}`}
       style={{
         maxWidth: "calc(100vw - 2rem)",
         wordBreak: "break-word",
