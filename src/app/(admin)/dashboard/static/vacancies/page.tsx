3bbe027145c9f6586795329c07ced194303cc851
chapppington <stas.mega.96@bk.ru>
2025-06-27 20:20:21 +0300

    static content editor placeholder made

---
+++ src/app/(admin)/dashboard/static/vacancies/page.tsx
@@ -0,0 +1,8 @@
+export default function StaticVacanciesPage() {
+  return (
+    <div className="mt-20 max-w-2xl mx-auto text-left">
+      <h1 className="text-3xl font-bold mb-4">Вакансии</h1>
+      <div className="text-muted-foreground text-2xl">В разработке</div>
+    </div>
+  );
+}
