55c986e87a8b0dffe0d460f41436f4220c05869d
chapppington <stas.mega.96@bk.ru>
2025-06-28 09:56:52 +0300

    contants update

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_MAIN = "https://testsk2.ru/api/api";
+export const BACKEND_MAIN = "https://testsk2.ru/api";
 export const API_URL = `${BACKEND_MAIN}`;
-export const UPLOADS_URL = `https://testsk2.ru/api`;
+export const UPLOADS_URL = `https://testsk2.ru`;
