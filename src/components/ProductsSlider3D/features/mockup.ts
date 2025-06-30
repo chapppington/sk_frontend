98958adaf77fa9ee12522ae045175df5462ec9d2
chapppington <stas.mega.96@bk.ru>
2025-06-30 20:56:09 +0300

    catalog static content change

--- src/components/ProductsSlider3D/features/mockup.ts
+++ src/components/ProductsSlider3D/features/mockup.ts
@@ -1,41 +1,33 @@
 interface IMockup {
-    id: number;
-    name: string;
-    description: string;
-    modelPath: string;
+  id: number;
+  name: string;
+  description: string;
+  modelPath: string;
 }
 
 export const mockup: IMockup[] = [
-    {
-        id: 1,
-        name: "ПАРН",
-        description: "Пункт автоматического регулирования напряжения (ПАРН)",
-        modelPath: "/Scene/parn.glb"
-    },
-    {
-        id: 2,
-        name: "ПАРН2",
-        description: "Пункт автоматического регулирования напряжения (ПАРН)",
-        modelPath: "/Scene/parn2.glb"
-    },
-    {
-        id: 3,
-        name: "Ктп на столбе",
-        description: "Пункт автоматического регулирования напряжения (ПАРН)",
-        modelPath: "/Scene/ktpStolb.glb"
-    },
-    {
-        id: 4,
-        name: "2КТПБУ",
-        description: "Пункт автоматического регулирования напряжения (ПАРН)",
-        modelPath: "/Scene/ktpbu.glb"
-    },
-    
-
-    
-    
-
-    
-    
-
-]
+  {
+    id: 1,
+    name: "ПАРН",
+    description: "Пункт автоматического регулирования напряжения (ПАРН)",
+    modelPath: "/Scene/parn.glb",
+  },
+  {
+    id: 2,
+    name: "КТП",
+    description: "Комплектная трансформаторная подстанция (КТП)",
+    modelPath: "/Scene/parn2.glb",
+  },
+  {
+    id: 3,
+    name: "КТПМ",
+    description: "Комплектная трансформаторная подстанция мачтового типа(КТПМ)",
+    modelPath: "/Scene/ktpStolb.glb",
+  },
+  {
+    id: 4,
+    name: "2КТПБУ",
+    description: "Двухтрансформаторная комплектная трансформаторная подстанция блочного уличного исполнения.",
+    modelPath: "/Scene/ktpbu.glb",
+  },
+];
