e9a834aeb92bc6e4f6e4335c8ecad5802c70d43c
chapppington <stas.mega.96@bk.ru>
2025-06-30 20:37:40 +0300

    change of local scene (idk wtf)

--- src/components/3DScene/main.tsx
+++ src/components/3DScene/main.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Canvas, extend } from "@react-three/fiber";
+import React, { useMemo } from "react";
+import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
+import { AdaptiveDpr } from "@react-three/drei";
 import Scene from "@/components/3DScene/features/3dScene/Scene";
 import Camera3D from "@/components/3DScene/features/3dScene/Camera3D";
 
@@ -42,31 +44,19 @@ const MainScene = React.memo(() => {
     <div className="app-container fixed z-[-10]">
       <div className="canvas-container pointer-events-auto">
         <Canvas
-          style={{
-            height: "100vh",
-            width: "100vw",
-            position: "absolute",
-            top: 0,
-          }}
-          camera={{ position: [0, 0, 10], fov: 70, near: 10.1, far: 5000 }}
-          scene={{ background: new THREE.Color("#0f0d1f") }}
-          gl={{
-            antialias: true,
-            powerPreference: "high-performance",
-            stencil: false,
-            depth: false,
-            outputColorSpace: THREE.SRGBColorSpace,
-            alpha: true, // Enable alpha for transparency
-            logarithmicDepthBuffer: true, // Enable logarithmic depth buffer for better depth precision
-          }}
-          dpr={[1, 2]}
+          style={canvasSettings.style}
+          camera={canvasSettings.camera}
+          scene={canvasSettings.scene}
+          gl={canvasSettings.gl}
+          dpr={canvasSettings.dpr}
         >
+          {/* <AdaptiveDpr pixelated /> */}
           <Scene />
           <Camera3D />
         </Canvas>
       </div>
     </div>
   );
-}
+});
 
 export default MainScene;
