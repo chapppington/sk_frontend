df353957edbf08c5d6ee52cb045768525c99d2d2
chapppington <stas.mega.96@bk.ru>
2025-07-02 12:58:25 +0300

    mapping changed

--- src/app/(admin)/dashboard/news/components/NewsDialog.tsx
+++ src/app/(admin)/dashboard/news/components/NewsDialog.tsx
@@ -96,9 +96,10 @@ const NewsDialog: React.FC<NewsDialogProps> = ({
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="production">Производство</SelectItem>
-                  <SelectItem value="technology">Технологии</SelectItem>
-                  <SelectItem value="event">События</SelectItem>
-                  <SelectItem value="interview">Интервью</SelectItem>
+                  <SelectItem value="developments">Разработки</SelectItem>
+                  <SelectItem value="useful">Полезное</SelectItem>
+                  <SelectItem value="events">События</SelectItem>
+                  <SelectItem value="projects">Наши проекты</SelectItem>
                 </SelectContent>
               </Select>
             </div>
