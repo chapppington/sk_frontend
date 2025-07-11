ff3cc2a0c7736e19752c56c6bd438126abe8e345
chapppington <stas.mega.96@bk.ru>
2025-07-11 22:09:29 +0300

    separated row component

--- src/app/(admin)/dashboard/products/components/OrderTable.tsx
+++ src/app/(admin)/dashboard/products/components/OrderTable.tsx
@@ -3,82 +3,18 @@
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/shadcn/table";
-import Image from "next/image";
-import { BACKEND_MAIN, UPLOADS_URL } from "@/constants";
 import { GripVertical } from "lucide-react";
 import { DndContext, closestCenter } from "@dnd-kit/core";
 import {
-  arrayMove,
   SortableContext,
-  useSortable,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
 import { IProduct } from "@/shared/types/product.types";
-import { Badge } from "@/components/ui/shadcn/badge";
-
-function SortableProductRow({
-  product,
-  order,
-}: {
-  product: IProduct;
-  order: number;
-}) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id: product.id });
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-  return (
-    <TableRow
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className={`cursor-grab active:cursor-grabbing transition-colors transition-shadow duration-200
-        ${
-          isDragging
-            ? "bg-accent/60 dark:bg-gray-800"
-            : "hover:bg-accent/40 dark:hover:bg-gray-800 hover:shadow-md"
-        }
-        `}
-    >
-      <TableCell className="w-8 text-center text-gray-400">{order}</TableCell>
-      <TableCell className="w-8">
-        <GripVertical className="text-gray-400 dark:text-gray-300 transition-transform duration-200 group-hover:scale-125 group-hover:-rotate-12" />
-      </TableCell>
-      <TableCell>
-        {product.previewImageUrl ? (
-          <div className="w-16 h-16 rounded-lg overflow-hidden border relative">
-            <Image
-              src={`${UPLOADS_URL}${product.previewImageUrl}`}
-              alt={product.name}
-              fill
-              className="object-contain"
-            />
-          </div>
-        ) : (
-          <div className="w-16 h-16 rounded-lg bg-gray-100 flex items-center justify-center border">
-            <span className="text-xs text-gray-500">Нет фото</span>
-          </div>
-        )}
-      </TableCell>
-      <TableCell className="font-medium">{product.name}</TableCell>
-    </TableRow>
-  );
-}
+import SortableProductRow from "./SortableProductRow";
 
 export default function OrderTable({
   orderItems,
