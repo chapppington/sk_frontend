a67737dc9e5ec6b0b2a4d664147ed631fee9e41b
chapppington <stas.mega.96@bk.ru>
2025-07-12 13:49:23 +0300

    transition fix

--- src/app/(admin)/dashboard/products/components/SortableProductRow.tsx
+++ src/app/(admin)/dashboard/products/components/SortableProductRow.tsx
@@ -31,7 +31,7 @@ export default function SortableProductRow({
       style={style}
       {...attributes}
       {...listeners}
-      className={`cursor-grab active:cursor-grabbing transition-colors transition-shadow duration-200
+      className={`cursor-grab active:cursor-grabbing transition-all duration-200
         ${
           isDragging
             ? "bg-accent/60 dark:bg-gray-800"
