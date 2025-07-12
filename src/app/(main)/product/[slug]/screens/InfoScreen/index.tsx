4deb3a3016eb46e5324bcc4c3665b1d974c70c00
chapppington <stas.mega.96@bk.ru>
2025-07-12 11:48:22 +0300

    alt fix

--- src/app/(main)/product/[slug]/screens/InfoScreen/index.tsx
+++ src/app/(main)/product/[slug]/screens/InfoScreen/index.tsx
@@ -126,7 +126,8 @@ const InfoScreen: FC<InfoScreenProps> = ({ product }) => {
                   <Image
                     src={`${UPLOADS_URL}/uploads/products/${product.advantages[previousFeatureIndex].image}`}
                     alt={
-                      product.advantages[previousFeatureIndex].alt || ""
+                      product.advantages[previousFeatureIndex].alt ||
+                      product.advantages[previousFeatureIndex].label
                     }
                     fill
                     className="object-cover rounded-lg"
