67ea13e8bca3e4097097fa548a96498c8e02a32f
chapppington <stas.mega.96@bk.ru>
2025-08-03 14:51:25 +0700

    made poster required input for portfolio

--- src/app/(admin)/dashboard/portfolio/components/PortfolioDialog.tsx
+++ src/app/(admin)/dashboard/portfolio/components/PortfolioDialog.tsx
@@ -210,6 +210,7 @@ export default function PortfolioDialog({
               <Input
                 type="file"
                 accept="image/*"
+                required
                 onChange={(e) =>
                   setFormData({
                     ...formData,
