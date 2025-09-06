67a8a30f258371c6d2aa9f83ab32f96bfabe0c49
feeeed <76104017+feeeed@users.noreply.github.com>
2025-09-06 16:26:47 +0600

    Add simple gradient background
    Simple gradient bg for software acceleration

--- src/components/3DScene/ConditionalMainScene.tsx
+++ src/components/3DScene/ConditionalMainScene.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import MainScene from "./main";
 import useIsAppleDevice from "@/hooks/useIsAppleDevice";
 import useIsMobile from "@/hooks/useIsMobile";
+import { SimpleBackgroundGradient } from '@/components/ui/SimpleBackgroundGradient';
 import { useGPUContext } from "@/context/GpuDetectProvider";
 
 export default function ConditionalMainScene() {
@@ -16,11 +17,13 @@ export default function ConditionalMainScene() {
 
   // Memoize the MainScene instance
   const memoizedScene = useMemo(() => <MainScene />, []);
+  const simpleGradient = useMemo(() => <SimpleBackgroundGradient />, []);
 
   // Only show MainScene on home page for non-Apple devices with large screens
   if (isHomePage && context.webglSupport.isHardwareAccelerated && !isAppleDevice && !isSmallScreen) {
     return memoizedScene;
   }
+  if (isHomePage && !context.webglSupport.isHardwareAccelerated) return simpleGradient;
 
   return null;
 }
--- src/components/ui/BackgroundGradient/ConditionalBackgroundGradient.tsx
+++ src/components/ui/BackgroundGradient/ConditionalBackgroundGradient.tsx
@@ -4,8 +4,9 @@ import { FC } from "react";
 import { usePathname } from "next/navigation";
 import useIsAppleDevice from "@/hooks/useIsAppleDevice";
 import useIsMobile from "@/hooks/useIsMobile";
-
+import { useGPUContext } from "@/context/GpuDetectProvider";
 import BackgroundGradient from "@/components/ui/BackgroundGradient";
+import { SimpleBackgroundGradient } from '@/components/ui/SimpleBackgroundGradient';
 
 const ConditionalBackgroundGradient: FC = () => {
   const pathname = usePathname();
@@ -15,10 +16,12 @@ const ConditionalBackgroundGradient: FC = () => {
 
   // Show BackgroundGradient on non-home pages, or on home page for Apple devices/small screens
   const shouldShowGradient = !isHomePage || isAppleDevice || isSmallScreen;
+  const context = useGPUContext();
 
   if (!shouldShowGradient) return null;
 
-  return <BackgroundGradient />;
+  if (!context.webglSupport.isHardwareAccelerated) return <SimpleBackgroundGradient />;
+  if (context.webglSupport.isHardwareAccelerated) return <BackgroundGradient />;
 };
 
 export default ConditionalBackgroundGradient;
