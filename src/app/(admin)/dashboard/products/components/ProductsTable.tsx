9f2043c0c474d8d835c1441cff3b1bb335086c7b
chapppington <stas.mega.96@bk.ru>
2025-09-11 10:54:11 +0700

    adv fix 2

--- src/app/(admin)/dashboard/products/components/ProductsTable.tsx
+++ src/app/(admin)/dashboard/products/components/ProductsTable.tsx
@@ -157,13 +157,11 @@ export default function ProductsTable({
                   <span
                     className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
                       product.showAdvantages ?? true
-                        ? "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200"
-                        : "bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200"
+                        ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200"
+                        : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200"
                     }`}
                   >
-                    {product.showAdvantages ?? true
-                      ? "Преимущества"
-                      : "Без преимуществ"}
+                    Преимущества
                   </span>
                 </div>
               </TableCell>
