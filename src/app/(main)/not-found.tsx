36d0e8316276350a0f2e9e50f08ee572d6464343
chapppington <stas.mega.96@bk.ru>
2025-09-30 11:50:33 +0700

    404 bg gradient fix

--- src/app/(main)/not-found.tsx
+++ src/app/(main)/not-found.tsx
@@ -1,9 +1,11 @@
 import NotFoundScreen from "@/components/shared_screens/NotFoundScreen";
+import ConditionalBGGradient from "@/components/ui/BackgroundGradient/ConditionalBackgroundGradient";
 import Providers from "../providers";
 
 export default function NotFoundPage() {
   return (
     <Providers>
+      <ConditionalBGGradient />
       <NotFoundScreen />
     </Providers>
   );
