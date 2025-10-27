88ac3537a711f3952aafccde6afbabdb51b09f86
chapppington <stas.mega.96@bk.ru>
2025-10-27 16:05:51 +0700

    yandex metrika change

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,6 @@ import Footer from "@/components/shared_screens/Footer";
 import ConditionalBGGradient from "@/components/ui/BackgroundGradient/ConditionalBackgroundGradient";
 import ConditionalMainScene from "@/components/3DScene/ConditionalMainScene";
 import CustomScrollbar from "@/components/CustomScrollbar";
-import YandexMetrika from "@/components/YandexMetrika";
 import "./globals.css";
 import { useNotification } from "@/components/NotificationSection/NotificationProvider";
 import { useGPUContext } from "@/context/GpuDetectProvider";
@@ -21,7 +20,6 @@ export default function MainLayout({ children }: { children: ReactNode }) {
 
   return (
     <>
-      <YandexMetrika />
       <ConditionalBGGradient />
       <CustomScrollbar />
       <ConditionalMainScene />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import { WebGLProvider } from "@/components/ui/BackgroundGradient/WebGLContext";
+import YandexMetrika from "@/components/YandexMetrika";
 
 export const metadata: Metadata = {
   title: "СибКомплект",
@@ -17,7 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <YandexMetrika />
+      </head>
       <body>
         <WebGLProvider>
           <Providers>{children}</Providers>
