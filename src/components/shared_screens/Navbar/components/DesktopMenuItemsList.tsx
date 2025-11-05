351fc20dce6bc465b7415b59abcc6a9acf0de28c
chapppington <stas.mega.96@bk.ru>
2025-11-05 11:16:14 +0700

    dropdown fix

--- src/components/shared_screens/Navbar/components/DesktopMenuItemsList.tsx
+++ src/components/shared_screens/Navbar/components/DesktopMenuItemsList.tsx
@@ -60,13 +60,13 @@ const DesktopMenu: FC = () => {
               onMouseEnter={() => setDropdownOpen(true)}
               onMouseLeave={() => setDropdownOpen(false)}
             >
-              <button
+              <TransitionLink
+                href={item.href}
                 className="text-white text-sm hover:text-white/80 transition-colors relative select-none group"
-                onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 {item.label}
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
-              </button>
+              </TransitionLink>
               
               {dropdownOpen && (
                 <div className="absolute top-full left-0 pt-2">
